d67aea4c525b0d42858db0552bac99ab6256acb6
KirillButkyavichus <Kirill191818@mail.ru>
2021-07-17 20:08:20 +0300

    Задание №1 выполнено

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
 
 
 console.log('Hello world!');
+
+const button = document.querySelector('.js_appointment-btn');
+console.log(button);
+button.addEventListener('click', function() {
+  const nameInput = document.querySelector('.js_appointment-input-name');
+  const emailInput = document.querySelector('.js_appointment-input-email');
+  const nameInputValue = nameInput.value;
+  const emailInputValue = emailInput.value;
+  const successMessage = document.querySelector('.js_appointment-success-message'); 
+  successMessage.classList.remove('appointment-success-message-active');
+
+  if (nameInputValue==="") {
+    const nameInputError = document.querySelector('.js_appointment-input-name-error');
+    nameInputError.classList.add('input__error-active')
+  }
+
+  if (emailInputValue==="") {
+    const nameInputError = document.querySelector('.js_appointment-input-email-error');
+    nameInputError.classList.add('input__error-active');
+  }
+
+  if (nameInputValue!=="" && emailInputValue!=="") {
+    successMessage.classList.add('appointment-success-message-active');
+    nameInput.value = "";
+    emailInput.value = "";
+    const formData = {
+      email: emailInputValue,
+      fullname: nameInputValue   
+    }
+    console.log(formData);
+  }
+}) 
+
